9c6a9eca75611bcca219993e326b656f5eba3fce
David Vite <vitebasket10@gmail.com>
2016-06-01 11:00:11 -0600

    Change computer, taking the last changes.

--- break_time.py
+++ break_time.py
@@ -1,11 +1,11 @@
 import webbrowser
 import time
 
-total_breaks = 4
+total_breaks = 1
 break_count = 1
 
 print ("this program started on "+time.ctime())
 while (break_count <= total_breaks):
-        time.sleep(60*60)
+        time.sleep(1)
         webbrowser.open("https://youtu.be/sHE0wmgljco?t=52s")
         break_count = break_count + 1
---
+++ child.py
@@ -0,0 +1,10 @@
+import inheritance
+
+# david_vite = inheritance.Parent("Vite","Black")
+# print(david_vite.last_name)
+
+jacob_vite = inheritance.Child("Vite", "Blue", 5)
+
+
+print(jacob_vite.last_name)
+print(jacob_vite.number_of_toys)
